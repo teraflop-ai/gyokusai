9df64ae131504cd5383f0b1a468675a6122dddaa
Enrico Shippole <enricoship@gmail.com>
2026-09-16 16:16:42 -0400

    needs to support input and output columns for classifiers

--- src/gyokusai/classifiers/stages.py
+++ src/gyokusai/classifiers/stages.py
@@ -7,11 +7,15 @@ from .regexes import FASTTEXT_REGEX
 
 
 class Whichlang:
-    def __init__(self):
+    def __init__(self, input_column: str = "text", output_column: str = "language"):
+        self.input_column = input_column
+        self.output_column = output_column
         self.predictor = WhichlangPredictor()
 
     def __call__(self, df: DataFrame) -> DataFrame:
-        return df.with_column("language", self.predictor.predict(col("text")))
+        return df.with_column(
+            self.output_column, self.predictor.predict(col(self.input_column))
+        )
 
 
 class NSFW:
@@ -19,20 +23,32 @@ class NSFW:
         self,
         model_repo_id="allenai/dolma-jigsaw-fasttext-bigrams-nsfw",
         model_filename="model.bin",
+        input_column: str = "text",
+        output_column: str = "nsfw_score",
     ):
         model_path = hf_hub_download(repo_id=model_repo_id, filename=model_filename)
+        self.input_column = input_column
+        self.output_column = output_column
         self.predictor = NSFWPredictor(model_path)
 
     def __call__(self, df: DataFrame) -> DataFrame:
-        cleaned = F.strip(F.regexp_replace(col("text"), FASTTEXT_REGEX, " "))
-        return df.with_column("nsfw_score", self.predictor.predict(cleaned))
+        cleaned = F.strip(F.regexp_replace(col(self.input_column), FASTTEXT_REGEX, " "))
+        return df.with_column(self.output_column, self.predictor.predict(cleaned))
 
 
 class LanguageID:
-    def __init__(self, model_repo_id="cis-lmu/glotlid", model_filename="model_v3.bin"):
+    def __init__(
+        self,
+        model_repo_id="cis-lmu/glotlid",
+        model_filename="model_v3.bin",
+        input_column: str = "text",
+        output_column: str = "language",
+    ):
         model_path = hf_hub_download(repo_id=model_repo_id, filename=model_filename)
+        self.input_column = input_column
+        self.output_column = output_column
         self.predictor = LanguagePredictor(model_path)
 
     def __call__(self, df: DataFrame) -> DataFrame:
-        cleaned = F.strip(F.regexp_replace(col("text"), FASTTEXT_REGEX, " "))
-        return df.with_column("language", self.predictor.predict(cleaned))
+        cleaned = F.strip(F.regexp_replace(col(self.input_column), FASTTEXT_REGEX, " "))
+        return df.with_column(self.output_column, self.predictor.predict(cleaned))
